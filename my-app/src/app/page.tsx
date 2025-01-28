e0acf173c204052ae21a8f8994fa0b12bc31ef09
mk <kanzakimiki.12@gmail.com>
2025-01-29 00:04:31 +0900

    sort 比較　完成

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -8,47 +8,68 @@ import { useEffect, useState } from "react";
 
 const Home: React.FC = () => {
   const [data, setData] = useState<User[]>([]);
-  // const [startData, setStartData] = useState<User[]>([]);
+  const [startData, setStartData] = useState<User[]>([]);
+  const [timeO, setTimeO] = useState<number | null>(null);
+  const [timeB, setTimeB] = useState<number | null>(null);
+
   useEffect(() => {
     // ダミーデータを生成
-    const newDummyData = dummyData(80); // 例: 100件生成
+    const newDummyData = dummyData(10); // 例: 100件生成
     setData(newDummyData); // 状態を更新
-    // setStartData(newDummyData);
+    setStartData(newDummyData);
     console.log(newDummyData);
   }, []);
 
   //リセット
-  // const reset = () => {
-  //   setData([...startData]);
-  //   console.log("reset");
-  // };
+  const reset = () => {
+    setData([...startData]);
+    console.log("reset");
+  };
 
   //年齢順でソート(Object)
   const ageSort_ob = () => {
+    const start = performance.now(); // 計測開始
+
     const sortedAge = sortObject(data);
     setData([...sortedAge]);
-    console.log(sortedAge);
+    const end = performance.now(); //計測終了
+    setTimeO(end - start);
   };
 
   //年齢順でソート(Bubble)
   const ageSort_bu = () => {
+    const start = performance.now(); // 計測開始
+
     const sortedAge = sortBubble(data);
     setData([...sortedAge]);
-    console.log("bubble click");
+    const end = performance.now(); //計測終了
+    setTimeB(end - start);
   };
 
   return (
     <div className={style.mainContainer}>
-      <h1 className={style.title}>User List (data:{data.length}) </h1>
-      <div className={style.btnContainer}>
-        <button className={style.resetBtn}>RESET</button>
-        <button className={style.btnAge} onClick={ageSort_ob}>
-          年齢順(Object)
-        </button>
-        <button className={style.btnAge} onClick={ageSort_bu}>
-          年齢順(Bubble)
-        </button>
-      </div>
+      <header className={style.header}>
+        <h1 className={style.title}>User List (data:{data.length}) </h1>
+        <div className={style.btnContainer}>
+          <button className={style.resetBtn} onClick={reset}>
+            RESET
+          </button>
+          <button className={style.btnAge} onClick={ageSort_ob}>
+            年齢順(Object)
+          </button>
+          <button className={style.btnAge} onClick={ageSort_bu}>
+            年齢順(Bubble)
+          </button>
+        </div>
+
+        <div className={style.timeContainer}>
+          {/* <p>Object計測時間：{(timeO ? timeO / 1000 : 0).toFixed(2)}ms</p>
+          <p>Bubble計測時間：{(timeB ? timeB / 1000 : 0).toFixed(2)}ms</p> */}
+
+          <p>Object計測時間：{(timeO ? timeO : 0).toFixed(2)}ms</p>
+          <p>Bubble計測時間：{(timeB ? timeB : 0).toFixed(2)}ms</p>
+        </div>
+      </header>
 
       <ul className={style.listContainer}>
         {data.map((user) => (
